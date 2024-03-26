87f649e3efdda6813d770a6fdecd9610c894fadc
yulin0625 <yulin90625@gmail.com>
2024-03-26 22:37:35 +0800

    Display Category Items on the Screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      home: const CategoriesScreen(),
     );
   }
 }
