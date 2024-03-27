1a37e9596d626a11fe93384336f4bc40884fb288
yulin0625 <yulin90625@gmail.com>
2024-03-27 16:24:11 +0800

    Fix the path error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:meals/widgets/screens/categories.dart';
+import 'package:meals/screens/categories.dart';
 
 
 final theme = ThemeData(
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/widgets/category_grid_item.dart';
-import 'package:meals/widgets/screens/meals.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
