c0b06b3b82b40ab34a09ba4fbee9bca70c428c14
yulin0625 <yulin90625@gmail.com>
2024-03-27 15:18:48 +0800

    Load meals data into a screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals/widgets/screens/categories.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +25,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      // home: const CategoriesScreen(),
+      home: const MealsScreen(
+        title: 'Some category',
+        meals: dummyMeals,
+      ),
     );
   }
 }
--- lib/widgets/screens/meals.dart
+++ lib/widgets/screens/meals.dart
@@ -13,10 +13,42 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) => Text(
+          meals[index].title,
+          style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
+      body: content,
     );
   }
 }
