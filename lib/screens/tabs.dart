a15afbad7cdd8922affae62b9ff83f6b351da81e
yulin0625 <yulin90625@gmail.com>
2024-04-09 19:52:53 +0800

    Add tab-based navigation

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabScreen extends StatefulWidget {
+  const TabScreen({super.key});
+  @override
+  State<TabScreen> createState() {
+    return _TabScreenState();
+  }
+}
+
+class _TabScreenState extends State<TabScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    var activePageTitle = 'Categories';
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: []);
+      activePageTitle = 'Your Favorites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex, // 目前所選取的 index, 會將其 highlight
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites')
+        ],
+      ),
+    );
+  }
+}
